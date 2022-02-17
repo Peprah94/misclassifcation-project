a9e355adc0bc6a23f66431c6c3700f16d99c8b3f
Peprah94 <31469850+Peprah94@users.noreply.github.com>
2022-02-17 08:42:08 +0100

    Update and rename all_sim.R to estimate_sim.R

--- Data simulation/estimate_sim.R
+++ Data simulation/estimate_sim.R
@@ -1,4 +1,9 @@
+#This code is used to estimate the parameters for the model from the simulated data
+
+#Load the simulated data from the "simulation.R" code
 load("simulated data1.RData")
+
+# Load packages needed to run the code
 library(pbapply)
 library(nimble)
 library(MCMCglmm)
@@ -10,26 +15,14 @@ library(extraDistr)
 library(MASS)
 library(spdep)
 library(parallel)
+require(ggmcmc)
 
-
-#source("maternfnx.R")
-
-#index <- function(y){
- # x <- nimC(y)
-#  return(which(x==1))
-#}
-#Rindex <- nimbleRcall(function(y=double(1)){}, Rfun="index", returnType = double(0))
-
-# Nimble function for estimating the proportions
-#nim_proportion <- function(y){
-  #x <- nimC(y)
-  #z <- (x)
-  #ret <- proportions(z)
- # return(ret)
-#}
-#nimbleProportion <- nimbleRcall(function(y=double(1)){}, Rfun="nim_proportion", returnType = double(1))
-
+# Function that estimates the parameters
+#Needed to run the code in parallel
 run_simulations <- function(simulations_all){
+  start_time <- Sys.time()
+  
+  # packages copied within the code for the parallelisation
   library(nimble)
   library(MCMCglmm)
   library(coda)
@@ -39,63 +32,19 @@ run_simulations <- function(simulations_all){
   library(extraDistr)
   library(MASS)
   library(spdep)
+  require(ggmcmc)
 
-
-  #source("maternfnx.R")
-  
-  #index <- function(y){
-   # x <- nimC(y)
-   # return(which(x==1))
- # }
- # Rindex <- nimbleRcall(function(y=double(1)){}, Rfun="index", returnType = double(0))
-  
-  # Nimble function for estimating the proportions
- # nim_proportion <- function(y){
-  #  x <- nimC(y)
-   #z <- (x)
-    #ret <- proportions(z)
-    #return(ret)
-  #}
-  #nimbleProportion <- nimbleRcall(function(y=double(1)){}, Rfun="nim_proportion", returnType = double(1))
-
-#The code for the MCMC
 code <- nimbleCode({
-  
-  
   # Priors for beta
   for(spe.tag in 1:n.species){
-    #for(cov.tag in 1:(n.cov+1)){
-    #beta[spe.tag,cov.tag] ~ dnorm(0, sd=100)
-    #beta[spe.tag,1] ~ dnorm(0, sd=100)
-    #mean.lam[spe.tag]~dunif(0,20)
-    beta0[spe.tag] ~ dnorm(0,sd=1)
-   # beta0[spe.tag]~dnorm(0, sd=10)
+ beta0[spe.tag] ~ dnorm(0,sd=1)
     beta1[spe.tag] ~ dnorm(0, sd=1)
-    #beta2[spe.tag] ~ dnorm(0, sd=1)
-    #}
   }
-  
-  
-  #Prior for spatial field
-  #tau ~dgamma(1,1) #Chi-square
-  #v.eta ~ T(dnorm(0, 0.1), 0, ) # I(0,)
-  #tau <- 1/v.eta
-  
-  
-  # Random Effects for the model
-  # sig_sites ~ dgamma(1,1)
-  # mu_sites ~ dnorm(0, 1)
-  #for(site.tag in 1:n.sites){
-  #alpha_sites[site.tag] ~ dnorm(mu_sites, sig_sites)
-  #alpha_sites[1:n.sites] ~ dcar_normal(adj[1:L], weights[1:L], num[1:n.sites], tau, zero_mean = 1)  
-  #}
-  
+
   # Linear Predictor
   for(spe.tag in 1:n.species){
     for(site.tag in 1:n.sites){
-      #log(lambda[spe.tag,site.tag]) <- beta[spe.tag,1] +  alpha_sites[site.tag]
-      linpred[spe.tag,site.tag] <-  beta0[spe.tag] + beta1[spe.tag]*cov[1,site.tag] #+ beta2[spe.tag]*cov[2,site.tag]
-      #lim.linpred[spe.tag,site.tag] <- min(1000, max(-1000, linpred[spe.tag,site.tag])) #stabilize the log
+      linpred[spe.tag,site.tag] <-  beta0[spe.tag] + beta1[spe.tag]*cov[site.tag] 
      lambda[spe.tag,site.tag] <- exp(linpred[spe.tag,site.tag])
     }
   }
@@ -119,21 +68,14 @@ code <- nimbleCode({
     omega[spe.tag, 1:(n.species+1)] ~ ddirch(alpha[spe.tag,1:(n.species+1)])
   }
   
-  # Citizen Science observations
+  # Verified data
   for(site.tag in 1:n.sites){
     for(visit.tag in 1:n.visit){
       C[visit.tag, site.tag] ~ dcat(prop[1:n.species,site.tag])
     }
   }
-  
-  # Subsetting index of which species are present
-#for(visit.tag in 1:n.visit){
- # for(site.tag in 1:n.sites){
- #   numb[visit.tag,site.tag] <- C[visit.tag, site.tag]
- # }
- #}
-  
-  # Verified observations from ML classifications
+
+  # Reported observations
   for(visit.tag in 1:n.visit){
     for(site.tag in 1:n.sites){
       Y[visit.tag,site.tag] ~ dcat(omega[C[visit.tag,site.tag],1:(n.species+1)])
@@ -142,46 +84,20 @@ code <- nimbleCode({
   
 })
 
-# Retrieving data from the simulations
+# Retrieving data from the simulated data
 C <- simulations_all$C # Unverified citizen science observation
 Y <- simulations_all$Y #Verified Citizen science observation
 cov <- simulations_all$cov #Covariates
-
-#Preparing the spatial component of the data
-#locs <- cbind(simulations_all$locs$Var1, simulations_all$locs$Var2)
-#neigh <- dnearneigh(locs, d1=0, d2=sqrt(0.5)+1)
-#winnb <- nb2WB(neigh)
-#winnb <- simulations_all$winnb
-#L <- length(winnb$adj)
-#str(winnb)
-#Setting parameters
 data <- list(C,Y, cov)
+
+#Constants for the model
 dim_data <- dim(data[[1]])
 n.cov=ncol(cov)
-
-n.species <- max(c(C))
-#alpha <- rep(1, (n.species+1))
-
-# Constants for the MCMC model
 const <- list(n.sites=dim_data[2],
               n.species=max(c(data[[1]])),
-              #n.sites = dim_data[3], 
-              #n.species = (dim_data[1]), 
               n.visit=(dim_data[1]), 
-              # n.visit=1, #use only for data
               n.cov=n.cov
-              #L=L, 
-              #adj=winnb$adj, 
-              #num=winnb$num, 
-              #weights=winnb$weights
               )
-alpha <- matrix(NA, nrow=const$n.species, ncol=(const$n.species+1))
-for(spe.tag in 1:const$n.species){
-  for(k in 1:(const$n.species+1)){
-    alpha[spe.tag,k] <- 1
- }
-}
-#alpha <- rep(1, (const$n.species+1))
 
 # Data for the Model
 idm_data <- list(C = data[[1]], 
@@ -189,6 +105,12 @@ idm_data <- list(C = data[[1]],
                  cov=data[[3]])
 
 # Initials for the model
+alpha <- matrix(NA, nrow=const$n.species, ncol=(const$n.species+1))
+for(spe.tag in 1:const$n.species){
+  for(k in 1:(const$n.species+1)){
+    alpha[spe.tag,k] <- 1
+  }
+}
 omega <- matrix(NA, nrow=const$n.species,
                 ncol = (const$n.species)+1) 
 for(spe.tag in 1:(const$n.species)){
@@ -196,13 +118,9 @@ for(spe.tag in 1:(const$n.species)){
 }
 
 idm_inits <- function(){list(beta1 =rep(1, const$n.species),
-                             #beta2 = rep(1,const$n.species),
                              omega=omega,
                              beta0 = rep(1, const$n.species),
-                             #tau= 2,
-                             alpha = alpha
-                             #alpha_sites = rep(0, const$n.sites )
-)
+                             alpha = alpha)
 }
 initsList <- idm_inits()
 
@@ -249,24 +167,41 @@ mcmc.out <- runMCMC(cmcmc,
                     summary = TRUE, 
                     WAIC = FALSE)
 
+# Return the summary of MCMC results
 output <- mcmc.out$summary$all.chains
+end.time <- Sys.time()
+time_taken <- as.numeric(round(end.time-start_time,2))
+
+# Preparing the results for the extraction of Rhat
+mcmclist <- ggs(mcmc.out$samples)
+
+# Subset the omega and beta parameters
+aa <- mcmclist%>%
+  filter(grepl('omega|beta',Parameter))
+
+subset_parameters<- unique(aa$Parameter)
+
+# Rhat values
+subset_Rhat <- aa%>%
+  ggs_Rhat()
+Rhat_data <- subset_Rhat$data[,5]
+all_rhat <- all(Rhat_data < 1.05) #Rhat is less than 1.05
+N_over_rhat <-length(which(Rhat_data > 1.05))/length(subset_parameters) #Rhats over 1.04
+
 beta0 <- output[(1:const$n.species),2]
 beta1 <- output[((const$n.species+1):(const$n.species*2)),2]
 lambda<- matrix(output[((const$n.species*2)+1) : (((const$n.species*2))+ (const$n.species*(const$n.sites))),2], nrow=const$n.species, ncol=const$n.sites, byrow=FALSE)
 omega <- matrix(output[((((const$n.species*2))+ (const$n.species*(const$n.sites)))+1): ((((const$n.species*2))+ (const$n.species*(const$n.sites)))+(const$n.species*(const$n.species+1))),2], nrow=const$n.species, ncol=(const$n.species+1), byrow=FALSE)
-#prop <- matrix(output[(((((const$n.species*2))+ (const$n.species*(const$n.sites)))+(const$n.species*(n.species+1)))) :((((const$n.species*2))+ (const$n.species*(const$n.sites)))+(const$n.species*(n.species+1)))+ (const$n.species*(const$n.sites)),1], nrow=const$n.species, ncol=const$n.sites, byrow=FALSE)
 prop <-matrix(tail(output[,2],(const$n.species*(const$n.sites))),nrow=const$n.species, ncol=const$n.sites, byrow=FALSE)
-return(list(omega=omega, beta0=beta0, beta1 = beta1, prop=prop, lambda=lambda))
+return(list(omega=omega, beta0=beta0, beta1 = beta1, prop=prop, lambda=lambda,N_over_rhat=N_over_rhat, all_rhat = all_rhat))
 }
 
-
+#Number of clusters
 cl <- makeCluster(5)
 setDefaultCluster(cl)
 rep_estimates <- pblapply(sim, run_simulations, cl=cl)
-#rep_estimates <- pblapply(sim[[1]], function(x){
-#  est <- run_simulations(x)
-#}, cl=1)
-#rep_estimates <- run_simulations(sim[[1]])
+stopCluster(cl)
 
+#Save results
 save(rep_estimates, file="estimateddata_new.RData")
 

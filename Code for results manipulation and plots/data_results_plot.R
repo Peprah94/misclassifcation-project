38aea106403d23d7da6aa0812a10fede9adcac25
Peprah94 <31469850+Peprah94@users.noreply.github.com>
2022-02-17 12:47:42 +0100

    Update and rename data_plot_alternative.R to data_results_plot.R

--- Code for results manipulation and plots/data_results_plot.R
+++ Code for results manipulation and plots/data_results_plot.R
@@ -1,5 +1,9 @@
-load("/Volumes/kwakupa/misclassification/data_results.RData")
-load("~/Documents/GitHub/Thesis/Misclassification/Misclassification/data_new.RData")
+# Load the data stored from "data formatting.R" and "data_application_nimble.R"
+
+load("data_results.RData") # results from data formatting.R
+load("data_new.RData")#results from data_application_nimble.R
+
+# packages needed to run this script
 library(ggplot2)
 library(pbapply)
 library(purrr)
@@ -10,41 +14,36 @@ unix::rlimit_as(100*10^9)
 world <- ne_countries(scale = "medium", returnclass = "sf")
 class(world)
 
-#const <- list(n.sites=1303,
-#             n.species=3,
-#n.sites = dim_data[3], 
-#n.species = (dim_data[1]), 
-#            n.reported=4)
-
+# Number of sites 
 n.sites = do.call('rbind',
                   pblapply(data_for_nimble,
                            function(x){
                              return(length(x$C))
                            }))
+# Number of verified species
 n.species = do.call('rbind',
                     pblapply(data_for_nimble,
                              function(x){
                                return(length(unique(c(x$C))))
                              }))
 
+#Number of reported species
 n.reported = do.call('rbind',
                      pblapply(data_for_nimble,
                               function(x){
                                 return(length(unique(c(x$Y))))
                               }))
 
+# Put all constants together
 const_all <- cbind(n.sites, n.species, n.reported)
 
+#Extract the results from the MCMC for each month
 monthly_mcmc.out <- list()
 for(i in 1:12){
   monthly_mcmc.out[[i]] <- rep_estimates[[i]][[9]]$summary$all.chains
 }
-#monthly_mcmc.out = do.call('rbind',
- #                          pblapply(rep_estimates,
- #                                   function(x){
- #                                     x[[9]]$summary$all.chains
- #                                   }))
 
+## Classification probabilities / Confusion matrix
 omega<- omega_full <- list()
 for(i in 1:12){
   const <- list(n.species=const_all[i,2],n.sites=const_all[i,1], n.reported=const_all[i,3])
@@ -67,7 +66,6 @@ if(nspecies > 2){
   omega[[i]] = rbind(omega_full[[i]],matrix(rep(0, (6)),ncol=(6),nrow=1))
 }
 }
-
 omega_res <- as.data.frame(do.call('rbind',omega))
 colnames(omega_res) <- c("Danaus gilippus", "Danaus plexippus", "Lemenitis archippus", "other", "other danaus", "other lemenitis")
 month <- rep(c("January", "February", "March",
@@ -78,7 +76,7 @@ species <- rep(c("Danaus gilippus", "Danaus plexippus", "Lemenitis archippus"),1
 misclassification_probabilities <- cbind(month, species, omega_res)
 write.csv(misclassification_probabilities, "misclassification_probability.csv")
 
-
+## Retrieving and saving results for the fixed effects beta0, beta1, beta2, beta3
 beta0_res <- do.call('rbind',
                      pblapply(rep_estimates, 
                               function(x){
@@ -129,6 +127,7 @@ betas_res <- cbind(month,beta0_res,
                    beta3_res)
 write.csv(betas_res, "betas.csv")
 
+# Function that estimates the proportion of the entire dataset.
 proportion_estimate <- function(data){
   ret <- matrix(NA, nrow=nrow(data), ncol = ncol(data))
   for(i in 1:nrow(data)){
@@ -137,7 +136,9 @@ proportion_estimate <- function(data){
   return(ret)
 }
 
-
+###########################################
+# Prediction for 2019 data that needed ID known as validation data
+##########################################
 results <- max_species <- list()
 for(month in 1:12){
   #for(i in 1:n.species){
@@ -153,12 +154,11 @@ for(month in 1:12){
   }
   pred_matrix <- proportion_estimate(cbind(t(pred1), t(pred2), t(pred3)))
   pred_prop_matrix <- pred_matrix/rowSums(pred_matrix)
+  
   res <- list()
   if(length(data_for_nimble[[month]]$validation_C) >0){
   for(i in 1:(length(data_for_nimble[[month]]$validation_Y))){
     print(i)
-    #iter <- month*i
-    #print(iter)
       coords_validation = data_for_nimble[[month]]$coords_validation
       res1 <-  pred_prop_matrix[i,]* omega[[month]][,(data_for_nimble[[month]]$validation_Y[i])]/sum(pred_prop_matrix[i,]* omega[[month]][,(data_for_nimble[[month]]$validation_Y[i])])
       max_species <- which.max(res1)
@@ -185,13 +185,12 @@ for(month in 1:12){
     }
   results[[month]] <- res
 }
-
-
 pred_probs = flatten(results)
 full_data <- data.frame(do.call("rbind", pred_probs))
 colnames(full_data) <- c("month", "Danaus gilipus", "Danaus plexippus", "Limenitis archippus", "Reported Species","truth", "max species","longitude", "latitude")
 sum(full_data$truth==full_data$`max species`)/nrow(full_data) #proportion of max species that correspond to the truth
 
+#save results
 write.csv(full_data, "results_from_data.csv")
 
 #monthly percentage of correct identifications
@@ -200,6 +199,7 @@ validation_prob <- full_data %>%
   dplyr:: group_by(month) %>%
   summarise(probability = mean(truth==`max species`, na.rm=TRUE) )
 
+#Plot of results
 melted_data <- reshape2::melt(full_data, id.vars=c("month","Reported Species","truth","max species","longitude", "latitude"))
 ggplot(data = world)+
   geom_sf()+
@@ -218,7 +218,9 @@ ggplot(data = world)+
   #facet_wrap(~month, ncol=3)+
   #theme(legend.position = "bottom")
 
-######################################
+###########################################
+# Prediction for 2020 research grade data
+##########################################
 results_20 <- list()
 for(month in 1:12){
   #for(i in 1:n.species){
@@ -274,8 +276,10 @@ research_2020 <-full_data %>%
  dplyr:: group_by(month) %>%
   summarise(probability = mean(truth==max_species, na.rm=TRUE) )
 
+#save the data
 write.csv(full_data, "results_from_data_2020.csv")
 
+#Plot the results
 melted_data <- reshape2::melt(full_data, id.vars=c("month","Reported Species","truth","max_species","longitude", "latitude"))
 gg1 <- list()
 for(i in 1:12){
@@ -305,7 +309,7 @@ ggarrange(gg1[[1]],gg1[[2]], gg1[[3]], gg1[[4]],
   theme(legend.position = "bottom")
  
  
- #Combine all probabilities
+ #Combine all probabilities and plot them
  all_probability <- rbind(validation_prob ,research_2020)
 groups <- as.factor(rep(c("needs ID 2019", "research grade 2020"), each=12))
 all_data <- cbind(groups,all_probability)
@@ -315,6 +319,6 @@ ggplot(data = all_data, aes(x=as.factor(month), y=probability,color=as.factor(gr
   xlab("Month")+
   ylab("Probability of correct classification")+
   theme(legend.title = element_blank(), legend.position = c(0.8,0.2))
-  #scale_fill_manual(values = c("white", "black"), labels=c("NS","S"), guide=guide_legend(reverse = TRUE))
+
 
             
